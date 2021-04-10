146d875641655bac10eb43177b3c74d2e3fef8a7
rumit0406 <rumit0406@gmail.com>
2021-04-10 16:28:04 +0530

    template v1

--- A.cpp
+++ A.cpp
@@ -43,6 +43,19 @@ void printpair(pii a) {
     cout << a.fi << " " << a.se << endl;
 }
 
+int ncr(int n, int r, int p = MOD) {
+    if (r > n - r)
+        r = n - r;
+    int C[r + 1];
+    fill(C, C + r + 1, 0);
+    C[0] = 1;
+    for (int i = 1; i <= n; i++) {
+        for (int j = min(i, r); j > 0; j--)
+            C[j] = (C[j] + C[j - 1]) % p;
+    }
+    return C[r];
+}
+
 void solve() {
 
 }
