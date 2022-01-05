8667e260ad18f72748f4c4f0e335ed8e377de97e
rumit garg <rumit0406@gmail.com>
2022-01-06 04:38:40 +0530

    debug template, clock, major changes done

--- A.cpp
+++ A.cpp
@@ -1,6 +1,43 @@
 #include <bits/stdc++.h>
+#include <chrono>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace chrono;
+using namespace __gnu_pbds;
+
+//debugging template
+//https://codeforces.com/blog/entry/68809
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i : x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+
 #define fastio() ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL)
 #define int long long
 #define double long double
@@ -19,15 +56,19 @@
 #define sci(x) int x; cin>>x
 #define scii(x,y) int x,y; cin>>x>>y
 #define sciii(x,y,z) int x,y,z; cin>>x>>y>>z
-#define print(x) fa(x) cout<<it<<" ";cout<<"\n"
 #define fi first
 #define se second
 #define yes cout<<"YES\n"
 #define no cout<<"NO\n"
-using namespace std;
-using namespace __gnu_pbds;
+
+//pbds
 typedef tree<int, null_type, less<int>, rb_tree_tag,
         tree_order_statistics_node_update> ordered_set;
+//order_of_key (k) : Number of items strictly smaller than k .
+// find_by_order(k) : K-th element in a set (counting from zero).
+//consider it an array, it gives a[k - 1]
+
+
 int MOD = 1e9 + 7, intmax = LLONG_MAX, intmin = LLONG_MIN;
 
 int power(int x, int y, int p = MOD) { //gives x^y, logy time
@@ -61,19 +102,8 @@ int ncr(int n, int r, int mod = MOD) {
 }
 
 void query(int a, int b) {
-	cout << "? " << a << ' ' << b << endl; cout.flush();
-}
-
-void print1d(v(int) a) {
-	fa(a)
-	cout << it << " ";
-	cout << endl;
-}
-
-void print2d(v(v(int)) &a) {
-	fa(a) {
-		print(it);
-	}
+	cout << "? " << a << ' ' << b << endl;
+	cout.flush();
 }
 
 void solve() {
@@ -81,6 +111,7 @@ void solve() {
 }
 
 signed main() {
+	auto start = high_resolution_clock::now();
 	fastio();
 	int t = 1;
 	cin >> t;
@@ -88,5 +119,8 @@ signed main() {
 		// cout << "Case #" << i << ": \n";
 		solve();
 	}
+	auto stop = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop - start);
+	cerr << "\nFinished in " << duration.count() << " µs!\n";
 	return 0;
 }
